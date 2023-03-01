7b54a1719bed3f15340da4f1d975c3b75821ed75
Juliette Cordor <juliette@jewelexx.com>
2023-03-01 18:09:36 +1100

    added simple boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,64 @@
-use bevy::{prelude::*, DefaultPlugins};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, DefaultPlugins};
 
-struct Ball {}
+#[derive(Default, Component, Deref, DerefMut)]
+struct Velocity(Vec2);
+
+#[derive(Component)]
+struct Ball {
+    pos: Vec3,
+    radius: f32,
+}
+
+impl Default for Ball {
+    fn default() -> Self {
+        Self {
+            pos: Self::STARTING_POS,
+            radius: Self::RADIUS,
+        }
+    }
+}
+
+impl Ball {
+    const STARTING_POS: Vec3 = Vec3::ZERO;
+    const RADIUS: f32 = 30.0;
+    const SIZE: Vec3 = Vec3::splat(Self::RADIUS);
+    const COLOUR: Color = Color::WHITE;
+}
+
+fn init_ball(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    // Ball
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::default().into()).into(),
+            material: materials.add(ColorMaterial::from(Ball::COLOUR)),
+            transform: Transform::from_translation(Ball::STARTING_POS).with_scale(Ball::SIZE),
+            ..default()
+        },
+        Ball::default(),
+        Velocity(Vec2::ZERO),
+    ));
+}
+
+fn apply_velocity(time: Res<Time>, mut query: Query<(&mut Transform, &Velocity)>) {
+    for (mut transform, velocity) in &mut query {
+        transform.translation.x += velocity.x * time.delta_seconds();
+        transform.translation.y += velocity.y * time.delta_seconds();
+    }
+}
+
+fn move_ball() {}
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(init_ball)
+        .add_system(apply_velocity)
+        .add_system(move_ball)
+        .run();
 }
