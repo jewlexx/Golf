689bf44080f7884be4604dac56f00a9830ab8d3c
Juliette Cordor <juliette@jewelexx.com>
2023-03-16 20:13:29 +1100

    fixes

--- src/levels/mod.rs
+++ src/levels/mod.rs
@@ -1,1 +1,24 @@
 pub mod def;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_toml_demo() {
+        let demo = include_str!("../../levels/demo.toml");
+
+        let level: def::Level = toml::from_str(demo).unwrap();
+
+        dbg!(level);
+    }
+
+    #[test]
+    fn test_parse_postcard_demo() {
+        let demo = include_bytes!("../../assets/levels/demo.bin");
+
+        let level: def::Level = postcard::from_bytes(demo).unwrap();
+
+        dbg!(level);
+    }
+}
